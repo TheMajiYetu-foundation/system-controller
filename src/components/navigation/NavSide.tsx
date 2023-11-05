a10907dd5b5ec29c484293e135b7ffb7fac875cd
Brian-mula <mulatiwangilabrian@gmail.com>
2023-11-05 13:33:29 +0300

    added navside

---
+++ src/components/navigation/NavSide.tsx
@@ -0,0 +1,14 @@
+
+export default function NavSide() {
+  return (
+    <div className="drawer-side">
+    <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> 
+    <ul className="menu p-4 w-64 min-h-full bg-base-200 text-base-content">
+      {/* Sidebar content here */}
+      <li><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li>
+    </ul>
+  
+  </div>
+  )
+}
