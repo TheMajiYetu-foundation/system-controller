9cd71d61b3270181062cec0e8b42a93ef9ed611c
Brian-mula <mulatiwangilabrian@gmail.com>
2023-11-05 13:33:18 +0300

    modified app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import HomeWrapper from "./ui/HomeWrapper";
 
 export default function App() {
   return (
-   <BrowserRouter>
-   <Routes>
-      <Route path="/" element={<Home />} />
-   </Routes>
-   </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route element={<HomeWrapper />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
