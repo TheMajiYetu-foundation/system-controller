6db952c80c9c8672b9cc70a359bd671ddfe47286
Brian-mula <mulatiwangilabrian@gmail.com>
2023-11-05 13:33:48 +0300

    added home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,10 @@
-
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="grid grid-col-1 grid-cols-2 px-10">
+      <div>Text</div>
+      <div>Text</div>
+      <div>Text</div>
+      <div>Text</div>
+    </div>
+  );
 }
