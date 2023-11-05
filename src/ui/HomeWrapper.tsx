ae82ea985af48ee82455a48e5255ee5719dcc905
Brian-mula <mulatiwangilabrian@gmail.com>
2023-11-05 13:33:59 +0300

    added homewrapper

---
+++ src/ui/HomeWrapper.tsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import NavSide from "../components/navigation/NavSide";
+import TopNav from "../components/navigation/TopNav";
+
+export default function HomeWrapper() {
+  return (
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content bg-base-100">
+        {/* Page content here */}
+        {/* Navbar */}
+        <TopNav />
+        <div className="px-3 pt-3">
+          <Outlet />
+        </div>
+      </div>
+      <NavSide />
+    </div>
+  );
+}
